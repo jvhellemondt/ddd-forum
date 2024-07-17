9cafab933d97cee32e14e427308faf9fd5cfc269
Jens van Hellemondt <jens@invictus.codes>
2024-07-18 00:09:17 +0200

    feat(api): add cors, etag, requestId and logger to api

--- src/shared/infrastructure/api/index.ts
+++ src/shared/infrastructure/api/index.ts
@@ -1,9 +1,14 @@
 import { healthRouter } from "@/common/useCases/getHealth";
 import { usersRouter } from "@/users";
 import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
+import { requestId } from "hono/request-id";
+import { etag } from "hono/etag";
 
 const app = new Hono();
 
+app.use("/*", cors(), etag(), requestId(), logger());
 app.route("/", healthRouter);
 app.route("/", usersRouter);
 
