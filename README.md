763c7819dc53b2b7bec437fdbdee69bcedcd7e77
Jens van Hellemondt <jens@invictus.codes>
2024-07-18 10:58:39 +0200

    docs(README): update commands

--- README.md
+++ README.md
@@ -1,15 +1,33 @@
 # ddd-forum
 
-To install dependencies:
+## To install dependencies:
 
 ```bash
 bun install
 ```
 
-To run:
+## To run:
 
 ```bash
-bun run src/main.ts
+bun run dev
 ```
+
+**Please note:**
+- the project is run with an in-memory sqlite database
+- the project is automatically seeded with (100) users
+
+## To test:
+
+```bash
+bun run test
+```
+
+## To end-to-end test (temporary):
+
+```bash
+./tests/curl.sh && ./tests/curl.sh
+```
+
+---
 
 This project was created using `bun init` in bun v1.1.20. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
--- tests/curl.sh
+++ tests/curl.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# Generated with ChatGPT. Ofcourse
+
 BASE_URL='http://localhost:3000'
 
 print_response() {
@@ -25,80 +27,115 @@ print_response() {
     echo "$body"
 }
 
+print_input() {
+    echo "Input Data:"
+    echo "$1" | jq .
+}
+
+print_request() {
+    method=$1
+    url=$2
+    echo "Method: $method"
+    echo "URL: $url"
+}
+
 echo "ERROR | it should return user not found / 404"
-response=$(curl --silent --location --write-out "\n%{http_code}" "${BASE_URL}/users?email=john%40EXAMPLE.c")
+print_request "GET" "${BASE_URL}/users?email=john%40EXAMPLE.com"
+response=$(curl --silent --location --write-out "\n%{http_code}" "${BASE_URL}/users?email=john%40EXAMPLE.com")
 print_response "$response"
 
 echo "ERROR | it should return user not found / 404"
+data='{
+    "lastName": "Snow"
+}'
+print_request "PUT" "${BASE_URL}/users/edit/101"
+print_input "$data"
 response=$(curl --silent --location --request PUT --write-out "\n%{http_code}" "${BASE_URL}/users/edit/101" \
 --header 'Content-Type: application/json' \
---data '{
-    "lastName": "Snow"
-}')
+--data "$data")
 print_response "$response"
 
 echo "ERROR | it should throw a validation error / 400"
-response=$(curl --silent --location --write-out "\n%{http_code}" "${BASE_URL}/users/new" \
---header 'Content-Type: application/json' \
---data-raw '{
+data='{
     "username": "john",
     "email": "john@example.com",
     "firstName": "John",
     "lastName": "Doe",
     "notValidField": "error"
-}')
+}'
+print_request "POST" "${BASE_URL}/users/new"
+print_input "$data"
+response=$(curl --silent --location --write-out "\n%{http_code}" "${BASE_URL}/users/new" \
+--header 'Content-Type: application/json' \
+--data-raw "$data")
 print_response "$response"
 
 echo "SUCCESS | it should create the user / 201"
-response=$(curl --silent --location --write-out "\n%{http_code}" "${BASE_URL}/users/new" \
---header 'Content-Type: application/json' \
---data-raw '{
+data='{
     "username": "john",
     "email": "john@example.com",
     "firstName": "John",
     "lastName": "Doe"
-}')
+}'
+print_request "POST" "${BASE_URL}/users/new"
+print_input "$data"
+response=$(curl --silent --location --write-out "\n%{http_code}" "${BASE_URL}/users/new" \
+--header 'Content-Type: application/json' \
+--data-raw "$data")
 print_response "$response"
 
 echo "ERROR | it should return username already in use / 409"
-response=$(curl --silent --location --write-out "\n%{http_code}" "${BASE_URL}/users/new" \
---header 'Content-Type: application/json' \
---data-raw '{
+data='{
     "username": "john",
     "email": "john-snow@example.com",
     "firstName": "John",
     "lastName": "Doe"
-}')
+}'
+print_request "POST" "${BASE_URL}/users/new"
+print_input "$data"
+response=$(curl --silent --location --write-out "\n%{http_code}" "${BASE_URL}/users/new" \
+--header 'Content-Type: application/json' \
+--data-raw "$data")
 print_response "$response"
 
 echo "ERROR | it should return email already in use / 409"
-response=$(curl --silent --location --write-out "\n%{http_code}" "${BASE_URL}/users/new" \
---header 'Content-Type: application/json' \
---data-raw '{
+data='{
     "username": "john-snow",
     "email": "john@example.com",
     "firstName": "John",
     "lastName": "Doe"
-}')
+}'
+print_request "POST" "${BASE_URL}/users/new"
+print_input "$data"
+response=$(curl --silent --location --write-out "\n%{http_code}" "${BASE_URL}/users/new" \
+--header 'Content-Type: application/json' \
+--data-raw "$data")
 print_response "$response"
 
 echo "SUCCESS | it should return the user / 200"
+print_request "GET" "${BASE_URL}/users?email=john%40EXAMPLE.com"
 response=$(curl --silent --location --write-out "\n%{http_code}" "${BASE_URL}/users?email=john%40EXAMPLE.com")
 print_response "$response"
 
 echo "ERROR | it should return validation error / 400"
+data='{
+    "admin": true
+}'
+print_request "PUT" "${BASE_URL}/users/edit/101"
+print_input "$data"
 response=$(curl --silent --location --request PUT --write-out "\n%{http_code}" "${BASE_URL}/users/edit/101" \
 --header 'Content-Type: application/json' \
---data '{
-    "admin": true
-}')
+--data "$data")
 print_response "$response"
 
 echo "SUCCESS | it should update the user / 201"
-response=$(curl --silent --location --request PUT --write-out "\n%{http_code}" "${BASE_URL}/users/edit/101" \
---header 'Content-Type: application/json' \
---data '{
+data='{
     "username": "winter-is-coming",
     "lastName": "Snow"
-}')
+}'
+print_request "PUT" "${BASE_URL}/users/edit/101"
+print_input "$data"
+response=$(curl --silent --location --request PUT --write-out "\n%{http_code}" "${BASE_URL}/users/edit/101" \
+--header 'Content-Type: application/json' \
+--data "$data")
 print_response "$response"
