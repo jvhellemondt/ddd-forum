381012f1150323750d6b91da3f56effbca073677
Jens van Hellemondt <jens@invictus.codes>
2024-07-18 09:31:01 +0200

    fix(users): add check on valid input createUser

--- src/modules/users/useCases/createUser/controller.ts
+++ src/modules/users/useCases/createUser/controller.ts
@@ -5,30 +5,28 @@ import { isObject } from "class-validator";
 import { isEmpty } from "lodash-es";
 import { Err, type Result } from "oxide.ts";
 import { CommonErrors } from "~/shared/common/errors";
-import type {
-	InsertUser,
-	User,
-} from "~/shared/infrastructure/database/models/users";
+import type { InsertUser, User } from "~/shared/infrastructure/database/models/users";
 import { type CreateUserModelErrors, createUserModel } from "./model";
 
-const hasUserInsertFields = (input: object) => {
-	const fields = ["email", "username", "firstName", "lastName"];
-	return fields.some((field) => isEmpty(input[field as keyof typeof input]));
+const insertUserAllowedFields = ["email", "username", "firstName", "lastName", "password"];
+
+const hasEveryInsertUserFields = (input: object) => {
+  return insertUserAllowedFields.every((field) => !isEmpty(input[field as keyof typeof input]));
 };
 
-const isUserInputValid = (input: unknown): input is InsertUser => {
-	if (isEmpty(input)) return false;
-	if (isObject(input) && hasUserInsertFields(input)) return false;
-	return true;
+const hasOnlyInsertUserFields = (input: object): input is InsertUser => {
+  return Object.keys(input).every((key) => insertUserAllowedFields.includes(key));
 };
 
-export type CreateUserControllerErrors =
-	| CommonErrors.ValidationError
-	| CreateUserModelErrors;
+const isUserInsertValid = (input: unknown): input is InsertUser => {
+  return isObject(input) && hasEveryInsertUserFields(input) && hasOnlyInsertUserFields(input);
+};
+
+export type CreateUserControllerErrors = CommonErrors.ValidationError | CreateUserModelErrors;
 
 export const createUserController = async (
-	body: Record<string, unknown>,
+  body: Record<string, unknown>,
 ): Promise<Result<User, CreateUserControllerErrors>> => {
-	if (!isUserInputValid(body)) return Err(CommonErrors.ValidationError);
-	return await createUserModel(body);
+  if (!isUserInsertValid(body)) return Err(CommonErrors.ValidationError);
+  return await createUserModel(body);
 };
